eb9da3b02015722e749ef0688e91545cebd35050
Naresh pc <Naresh pc@192.168.0.3>
2019-05-09 17:22:05 +0530

    3rd time change in class file

--- src/Mydata.java
+++ src/Mydata.java
@@ -8,6 +8,15 @@ public class Mydata {
 		 
 		this.empName = empName;
 	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+
 	
 	
 	
