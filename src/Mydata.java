8e26728247258d6d4e279d902ee0c0fa69e65949
Naresh pc <Naresh pc@192.168.0.3>
2019-05-09 17:15:14 +0530

    this is my 2nd change in program

--- src/Mydata.java
+++ src/Mydata.java
@@ -1,11 +1,11 @@
  
 public class Mydata {
     
-	private Integer empId;
+	 
 	private String empName;
 	public Mydata(Integer empId, String empName) {
 		super();
-		this.empId = empId;
+		 
 		this.empName = empName;
 	}
 	
