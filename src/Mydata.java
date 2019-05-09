0382236e968ca8975092082180e5136dfb187a21
Naresh pc <Naresh pc@192.168.0.3>
2019-05-09 17:12:13 +0530

    1st modification in class

--- src/Mydata.java
+++ src/Mydata.java
@@ -3,6 +3,11 @@ public class Mydata {
     
 	private Integer empId;
 	private String empName;
+	public Mydata(Integer empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
 	
 	
 	
